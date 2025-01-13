c6266d5af81bd634788c40010ddeb1e48cc2918e
Jnuka <jnuka@mail.ru>
2025-01-13 21:21:32 +0400

    feat: add calculateBalance()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -322,8 +322,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.flatMap((el) => el[0] - el[1]).reduce((sum, el) => sum + el, 0);
 }
 
 /**
